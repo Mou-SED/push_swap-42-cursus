9271e9e61e19eded9adbd9dc3f53deced8ce49c1
Mou-SED <moussa.seddik.ph@gmail.com>
2022-05-10 16:29:28 +0100

    add get_next_line to project

---
+++ lib/get_next_line/get_next_line.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moseddik <moseddik@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/23 18:22:42 by moseddik          #+#    #+#             */
+/*   Updated: 2022/05/10 14:17:56 by moseddik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# define BUFFER_SIZE 42
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strdup(const char *s1);
+size_t	ft_strlen(const	char	*s);
+char	*ft_strchr(char *str, int c);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+#endif
---
+++ lib/get_next_line/get_next_line_utils.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moseddik <moseddik@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/23 18:22:21 by moseddik          #+#    #+#             */
+/*   Updated: 2022/05/10 10:08:39 by moseddik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strchr(char *str, int c)
+{
+	int	i;
+
+	if (!str)
+		return (0);
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == (unsigned char)c)
+			return (&str[i]);
+		i++;
+	}
+	if (str[i] == (unsigned char)c)
+		return (&str[i]);
+	return (0);
+}
